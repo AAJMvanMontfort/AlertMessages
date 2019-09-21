89becddf6b5202fed23f54bca40c5096732b4020
AAJMvanMontfort <lout@me.com>
2019-09-21 14:51:23 +0200

    Initial Commit

---
+++ AlertMessages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlertMessages
+//
+//  Created by AAJM van Montfort on 21/09/2019.
+//  Copyright © 2019 AAJM van Montfort. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
